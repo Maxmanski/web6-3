0d2533e528a4e5ed7d87c99bddeb28cf465bb864
Nicole Haberberger <nicole.haberberger@gmail.com>
2015-05-06 13:38:07 +0200

    laden der daten aus json

--- we-lab3-group6/app/controllers/Overview.java
+++ we-lab3-group6/app/controllers/Overview.java
@@ -1,17 +1,30 @@
 package controllers;
 
+import at.ac.tuwien.big.we15.lab2.api.*;
+import at.ac.tuwien.big.we15.lab2.api.impl.*;
 import play.*;
 import play.mvc.*;
 
 import views.html.*;
 import play.i18n.*;
-import play.i18n.Lang;
 import play.i18n.Messages;
 import java.util.Formatter;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Overview extends Controller {
 
     public static Result index() {
+        String filepath ="/conf/data.de.json";
+        JeopardyFactory factory = new PlayJeopardyFactory(filepath);
+        QuestionDataProvider provider =factory.createQuestionDataProvider();
+        List<Category> categories = provider.getCategoryData(), tmp = new ArrayList<Category>();
+
+        User user = factory.createUser();
+        JeopardyGame game = factory.createGame(user);
+        Player human = game.getHumanPlayer();
+        
+
         return ok(index.render(Messages.get("label_titleQuestionSelection")));
     }
 
