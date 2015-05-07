44766b428fb30743be7f5d80eb49e0b1211a61d4
Maximilian Moser <maxmanski@live.at>
2015-05-07 21:28:20 +0200

    started implementing registration

--- we-lab3-group6/app/controllers/Registration.java
+++ we-lab3-group6/app/controllers/Registration.java
@@ -1,18 +1,52 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
-import play.i18n.*;
-import play.i18n.Lang;
+import model.User;
+import play.data.DynamicForm;
+import play.data.Form;
 import play.i18n.Messages;
-import java.util.Formatter;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.registration;
 
 public class Registration extends Controller {
 
     public static Result registration() {
-        return ok(registration.render(Messages.get("label_titleRegistration")));
+        Form<model.User> userForm = Form.form(model.User.class);
+        return ok(registration.render(Messages.get("label_titleRegistration"), userForm));
     }
 
+    public static Result register(){
+
+        DynamicForm form = Form.form().bindFromRequest();
+
+        String firstname, lastname, birthday, sex, avatar, username, password;
+
+        firstname = form.get("firstname");
+        lastname = form.get("lastname");
+        birthday = form.get("birthdate");
+        sex = form.get("gender");
+        avatar = form.get("avatar");
+        username = form.get("username");
+        password = form.get("password");
+
+        boolean valid = true;
+
+        if((username == null) || (username.length() < 4) || (username.length() > 8)){
+            valid = false;
+
+        }else if((password == null) || (password.length() < 4) || (password.length() > 8)){
+            valid = false;
+
+        }else if(firstname.equals("max")){
+            valid = false;
+        }
+
+        if(valid){
+            return redirect(controllers.routes.Authentication.authentication());
+
+        }else{
+            return redirect(controllers.routes.Registration.registration());
+
+        }
+    }
 }
---
+++ we-lab3-group6/app/model/User.java
@@ -0,0 +1,7 @@
+package model;
+
+/**
+ * Created by maxmanski on 07.05.15.
+ */
+public class User {
+}
